a94b24645f4aa1f3f90884418c2afccd1f821086
Arek Torczuk <torczuk@gmail.com>
2020-04-15 01:33:55 +0800

    Added test. Successful request should be stored and returned in case of the same clientId and requestNum

--- system_test/system_test.go
+++ system_test/system_test.go
@@ -14,3 +14,18 @@ func TestFirstClientRequest(t *testing.T) {
 	assert.Equal(t, res.Response, "Response: NoOp")
 	assert.Equal(t, res.RequestNum, uint32(1))
 }
+
+func TestCachedClientRequest(t *testing.T) {
+	c1 := &client.ReptileClient{"any-client", ":2600", 1}
+
+	res1, err := c1.Request("NoOp")
+	assert.Nil(t, err)
+	assert.Equal(t, res1.Response, "Response: NoOp")
+	assert.Equal(t, res1.RequestNum, uint32(1))
+
+	//same client id and request num, but different operations
+	c2 := &client.ReptileClient{"any-client", ":2600", 1}
+	res2, _ := c2.Request("Different")
+	assert.Equal(t, res2.Response, "Response: NoOp")
+	assert.Equal(t, res2.RequestNum, uint32(1))
+}
