a0b8ccd372f1d3cb2ba9b12effcab5754f2f9d4a
Arek Torczuk <torczuk@gmail.com>
2020-05-07 00:19:53 +0800

    Await - utility func for testing

---
+++ system_test/test_utils.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	logger "log"
+	"time"
+)
+
+type fn func() bool
+
+func await(condition fn, pool time.Duration, maxTime time.Duration) {
+	start := time.Now()
+	for !condition() {
+		time.Sleep(pool)
+		elapsed := time.Since(start)
+		if elapsed > maxTime {
+			logger.Fatalf("condition didn't finished within %v", maxTime)
+		}
+	}
+}
